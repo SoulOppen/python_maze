fbf601bee62c1078941a7938c49d7d4dea104cc3
SoulOppen <arieloppenheimer@outlook.cl>
2025-05-28 15:37:32 -0400

    fix:maze

---
+++ maze.py
@@ -0,0 +1,49 @@
+from cell import Cell
+import time
+
+class Maze:
+    def __init__(self,   
+        x1,
+        y1,
+        num_rows,
+        num_cols,
+        cell_size_x,
+        cell_size_y,
+        win):
+        self.x1 = x1
+        self.y1 = y1
+        self.num_rows = num_rows
+        self.num_cols = num_cols
+        self.cell_size_x = cell_size_x
+        self.cell_size_y = cell_size_y
+        self.win = win
+        self.__cells = []
+        
+        self.__create_cells()
+
+    def __create_cells(self):
+        for i in range(self.num_cols):
+            column = []  
+            for j in range(self.num_rows):
+
+                cell = Cell(self.win)
+                column.append(cell)
+            self.__cells.append(column) 
+        for i in range(self.num_cols):
+            for j in range(self.num_rows):
+                self.__draw_cell(i, j)
+    def __draw_cell(self, i, j):
+        x1 = self.x1 + i * self.cell_size_x
+        y1 = self.y1 + j * self.cell_size_y
+        x2 = x1 + self.cell_size_x
+        y2 = y1 + self.cell_size_y
+
+        cell = self.__cells[i][j]
+        cell.draw(x1, y1, x2, y2)
+        self._animate()
+
+    def _animate(self):
+        if self.win is None:
+            return
+        self.win.redraw()
+        time.sleep(0.05)

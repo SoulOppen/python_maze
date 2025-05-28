6a98f026594089dbd83ccb304d3162efc823fe3a
SoulOppen <arieloppenheimer@outlook.cl>
2025-05-28 11:12:54 -0400

    refactor:Classe

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
-from graphics import Window,Cell
+from cell import Cell
+from windows import Window
 def main():
     win = Window(800, 600)
-    cell1 = Cell(win)
-    cell1.draw(50, 50, 100, 100)
+    c1 = Cell(win)
+    c1.has_right_wall = False
+    c1.draw(50, 50, 100, 100)
 
-    cell2 = Cell(win)
-    cell2.draw(100, 50, 150, 100)
+    c2 = Cell(win)
+    c2.has_left_wall = False
+    c2.has_bottom_wall = False
+    c2.draw(100, 50, 150, 100)
 
-    cell1.draw_move(cell2)  # Movimiento normal
-    cell2.draw_move(cell1, undo=True)  # Retroceso en rojo
+    c1.draw_move(c2)
+
+    c3 = Cell(win)
+    c3.has_top_wall = False
+    c3.has_right_wall = False
+    c3.draw(100, 100, 150, 150)
+
+    c2.draw_move(c3)
+
+    c4 = Cell(win)
+    c4.has_left_wall = False
+    c4.draw(150, 100, 200, 150)
+
+    c3.draw_move(c4, True)
     win.wait_for_close()
 main()
